eb4cda32a91391b90f55969a3e22b749c5fed795
HVATA <HVATA@users.noreply.github.com>
2024-02-01 19:15:39 +0200

    Luokkien pohjat

---
+++ Varauksen_palvelut.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group9_VillageNewbies
+{
+    internal class Varauksen_palvelut
+    {
+    }
+}
