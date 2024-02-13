5e3c0281dc9dc19aae2c9fbb809c07a0b03db18f
HVATA <HVATA@users.noreply.github.com>
2024-02-13 14:39:02 +0200

    majava(majoitusvaraustenhallinta) headerin muutos

--- Majoitusvaraustenhallinta.Designer.cs
+++ Majoitusvaraustenhallinta.Designer.cs
@@ -51,10 +51,11 @@
             this.header.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.header.Location = new System.Drawing.Point(187, 9);
             this.header.Name = "header";
-            this.header.Size = new System.Drawing.Size(446, 39);
+            this.header.Size = new System.Drawing.Size(410, 39);
             this.header.TabIndex = 5;
-            this.header.Text = "Alueiden ja mökkien hallinta";
+            this.header.Text = "Majoitusvaraustenhallinta\r\n";
             this.header.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.header.Click += new System.EventHandler(this.header_Click);
             // 
             // Majoitusvaraustenhallinta
             // 
--- Majoitusvaraustenhallinta.cs
+++ Majoitusvaraustenhallinta.cs
@@ -16,5 +16,10 @@ namespace Group9_VillageNewbies
         {
             InitializeComponent();
         }
+
+        private void header_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
