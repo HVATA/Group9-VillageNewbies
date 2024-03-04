03f8a8f9a3f707dbef91f0077545b3d2cd54c729
Oskari Välipakka <s2308793@edu.savonia.fi>
2024-03-04 10:08:41 +0200

    Palvelut alku

--- Palveluhallinta.cs
+++ Palveluhallinta.cs
@@ -15,6 +15,43 @@ namespace Group9_VillageNewbies
         public Palveluhallinta ()
             {
             InitializeComponent();
+
+            DatabaseRepository repository = new DatabaseRepository();
+            DataTable palvelutTable = repository.ExecuteQuery("SELECT * FROM palvelu");
+
+            foreach (DataRow row in palvelutTable.Rows)
+                {
+                string palveluTiedot = string.Format("{0} {1}, {2}, {3}, {4}",
+                    row["palvelu_id"],
+                    row["nimi"],
+                    row["kuvaus"],
+                    row["hinta"],
+                    row["alv"]);
+
+                listBox1.Items.Add(palveluTiedot);
+                }    
+
+                
+            }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+            {
+
+            }
+
+        private void btnChange_Click(object sender, EventArgs e)
+            {
+
+            }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+            {
+
+            }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+            {
+                
             }
         }
     }
